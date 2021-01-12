5f1f090de2a2093da7d52acc5f18a6132e594d10
Lukentui <lukentuichennel@gmail.com>
2021-01-13 00:46:20 +0300

    src fix

--- src/offline-country-reverse-geocoder.php
+++ src/offline-country-reverse-geocoder.php
@@ -1,11 +1,7 @@
 <?php
-
 namespace DaveRoss\OfflineCountryReverseGeocoder;
 
-class FileException extends \Exception {
-}
-
-;
+class FileException extends \Exception {}
 
 /**
  * Retrieve an array of country codes & the corresponding coordinates
@@ -13,53 +9,44 @@ class FileException extends \Exception {
  * @return array
  * @throws FileException
  */
-function countries_array() {
+function countries_array()
+{
+  static $country_data;
 
-	// Memoize the data so its only read once
-	static $country_data;
+  if (!isset($country_data)) {
+    $country_data = [];
 
-	if ( ! isset( $country_data ) ) {
+    $fh = fopen(__DIR__ . '/polygons.properties', 'r');
+    if (!$fh) {
+      throw new FileException('Could not open the polygons.properties file');
+    }
 
-		$country_data = array();
+    while (!feof($fh)) {
+      $row = fgets($fh);
 
-		$fh = fopen( __DIR__ . '/polygons.properties', 'r' );
-		if ( ! $fh ) {
-			throw new FileException( 'Could not open the polygons.properties file' );
-		}
+      if (0 === strlen($row)) {
+        continue;
+      }
 
-		while ( ! feof( $fh ) ) {
+      $row = trim($row);
+      list($country_code, $data) = explode('=', $row, 2);
+      list($polygon_type, $data) = explode(' ', $data, 2);
+      $data = trim($data);
+      $data = trim($data, '(');
+      $data = trim($data, ')');
 
-			$row = fgets( $fh );
-			if ( 0 === strlen( $row ) ) {
-				continue;
-			}
+      if ('POLYGON' === strtoupper($polygon_type)) {
+        $country_data[] = array($country_code, $data);
+      } else if ('MULTIPOLYGON' === strtoupper($polygon_type)) {
+        $polygons = explode(')),((', $data);
+        array_walk($polygons, function ($polygon) use ($country_code, &$country_data) {
+          $country_data[] = array($country_code, $polygon);
+        });
+      }
+    }
+  }
 
-			$row = trim( $row );
-			list( $country_code, $data ) = explode( '=', $row, 2 );
-			list( $polygon_type, $data ) = explode( ' ', $data, 2 );
-			$data = trim( $data );
-			$data = trim( $data, '(' );
-			$data = trim( $data, ')' );
-
-			if ( 'POLYGON' === strtoupper( $polygon_type ) ) {
-
-				$country_data[] = array( $country_code, $data );
-
-			} else if ( 'MULTIPOLYGON' === strtoupper( $polygon_type ) ) {
-
-				$polygons = explode( ')),((', $data );
-				array_walk( $polygons, function ( $polygon ) use ( $country_code, &$country_data ) {
-					$country_data[] = array( $country_code, $polygon );
-				} );
-
-			}
-
-		}
-
-	}
-
-	return $country_data;
-
+  return $country_data;
 }
 
 /**
@@ -70,76 +57,74 @@ function countries_array() {
  *  Note that division by zero is avoided because the division is protected
  *  by the "if" clause which surrounds it.
  *
- * @param double $targetX
- * @param double $targetY
+ * @param float $targetX
+ * @param float $targetY
  * @param string $points_string
  *
  * @see http://alienryderflex.com/polygon/
  */
-function pointInPolygon( $targetX, $targetY, $points_string ) {
+function pointInPolygon(float $targetX, float $targetY, string $points_string)
+{
+  $points = explode(',', $points_string);
+  $polyCorners = count($points);
+  $polyX = [];
+  $polyY = [];
 
-	$points      = explode( ',', $points_string );
-	$polyCorners = count( $points );
-	$polyX       = array();
-	$polyY       = array();
+  foreach ($points as $point) {
+    list($pointX, $pointY) = explode(' ', $point);
 
-	foreach ( $points as $point ) {
+    $pointX = floatval($pointX);
+    $pointY = floatval($pointY);
 
-		list( $pointX, $pointY ) = explode( ' ', $point );
+    /**
+     * Performance optimization:
+     * If the first pair of coordinates are more than 90deg
+     * (1/4 of the Earth's circumference) in any direction,
+     * the answer is "no".
+     */
+    if ($targetY && (intval(abs($pointY - $targetY)) > 90)) {
+      return false;
+    }
+    if ($targetX && (intval(abs($pointX - $targetX)) > 90)) {
+      return false;
+    }
 
-		/**
-		 * Performance optimization:
-		 * If the first pair of coordinates are more than 90deg
-		 * (1/4 of the Earth's circumference) in any direction,
-		 * the answer is "no".
-		 */
-		if ( $targetY && ( intval( abs( $pointY - $targetY ) ) > 90 ) ) {
-			return false;
-		}
-		if ( $targetX && ( intval( abs( $pointX - $targetX ) ) > 90 ) ) {
-			return false;
-		}
+    $polyX[] = floatval($pointX);
+    $polyY[] = floatval($pointY);
+  }
 
-		$polyX[] = doubleval( $pointX );
-		$polyY[] = doubleval( $pointY );
+  $j = $polyCorners - 1;
+  $oddNodes = false;
 
-	}
+  for ($i = 0; $i < $polyCorners; $i++) {
+    if (($polyY[$i] < $targetY && $polyY[$j] >= $targetY
+        || $polyY[$j] < $targetY && $polyY[$i] >= $targetY)
+      && ($polyX[$i] <= $targetX || $polyX[$j] <= $targetX)
+    ) {
+      $oddNodes ^= ($polyX[$i] + ($targetY - $polyY[$i]) / ($polyY[$j] - $polyY[$i]) * ($polyX[$j] - $polyX[$i]) < $targetX);
+    }
+    $j = $i;
+  }
 
-	$j        = $polyCorners - 1;
-	$oddNodes = false;
-
-	for ( $i = 0; $i < $polyCorners; $i ++ ) {
-		if ( ( $polyY[ $i ] < $targetY && $polyY[ $j ] >= $targetY
-		       || $polyY[ $j ] < $targetY && $polyY[ $i ] >= $targetY )
-		     && ( $polyX[ $i ] <= $targetX || $polyX[ $j ] <= $targetX )
-		) {
-			$oddNodes ^= ( $polyX[ $i ] + ( $targetY - $polyY[ $i ] ) / ( $polyY[ $j ] - $polyY[ $i ] ) * ( $polyX[ $j ] - $polyX[ $i ] ) < $targetX );
-		}
-		$j = $i;
-	}
-
-	return $oddNodes;
+  return $oddNodes;
 }
 
 /**
  * Get the country code for a pair of lat/long coordinates
  *
- * @param double $longitude decimal longitude
- * @param double $latitude  decimal latitude
+ * @param float $longitude decimal longitude
+ * @param float $latitude decimal latitude
  *
- * @return string country code or empty
+ * @return string|null country code or null
  */
-function get_country( $longitude, $latitude ) {
+function get_country(float $longitude, float $latitude): ?string
+{
+  foreach (countries_array() as $country) {
+    list($country_code, $country_boundary) = $country;
+    if (pointInPolygon($longitude, $latitude, $country_boundary)) {
+      return $country_code;
+    }
+  }
 
-	$countries = countries_array();
-
-	foreach ( $countries as $country ) {
-		list( $country_code, $country_boundary ) = $country;
-		if ( pointInPolygon( $longitude, $latitude, $country_boundary, $country_code ) ) {
-			return $country_code;
-		}
-	}
-
-	return '';
-
+  return null;
 }
